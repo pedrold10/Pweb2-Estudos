80914e944f982607f51de5f6506ec38d79961903
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-04-04 09:29:06 -0300

    Avançando na atividade 03 - bitbank

--- workspacespring/bitbank/src/main/java/br/edu/ifpb/pweb2/bitbank/controller/HomeController.java
+++ workspacespring/bitbank/src/main/java/br/edu/ifpb/pweb2/bitbank/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/home")
     public String showHomePage(Model mav) {
         mav.addAttribute("menu", "home");
